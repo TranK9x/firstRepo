1e3b994bbbf6a2abb5e210e9adecd99d0609f769
TranK9x <vongkientran@gmail.com>
2024-09-13 23:21:34 +0000

    AAAAAAAAAAAA WHYWAHAYGWAYAWA today is friday

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util;
+
+class main{
+    public static void main(String[args]){
+        fade();
+    }
+    public fade(String name){
+        return name + " has a fresh fade!!!"
+    }
+}
